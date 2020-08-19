e10cd4780d18ba4a8de7f4bf728d7e7a771948ad
abouhid <alexandre.bouhid@engenharia.ufjf.br>
2020-08-19 11:03:57 -0300

    Start developing check_empty_lines

--- lib/errors.rb
+++ lib/errors.rb
@@ -1,24 +1,31 @@
-require_relative '../lib/check.rb'
-require_relative '../lib/read.rb'
-require 'strscan'
+require_relative "../lib/check.rb"
+require_relative "../lib/read.rb"
+require "strscan"
 
 module Errors
   def check_indentation(file_data)
-    counter = 0;
-    read_file = Read.new(file_data)
-    s = StringScanner.new(read_file.hash.to_s)
-
+    counter = 0
   end
 
   def check_spacing(file_data)
-    offenses << false
+    # offenses << false
   end
 
   def check_closing(file_data)
-    offenses << 3
+    # offenses << 3
   end
 
   def check_empty_lines(file_data)
-    offenses << 4
+    file_data.size.times do |i|
+      pl = i - 1
+      cl = i
+      nl = i + 1
+      if file_data[i] == "" && file_data[pl] == ""
+       puts "Line: #{i}:".blue + ' Warning: '.yellow + 'Layout/EmptyLines: Extra blank line detected.'
+       offenses << 1
+      end
+      # lib/errors.rb:28:1: C:
+
+    end
   end
 end
--- lib/read.rb
+++ lib/read.rb
@@ -3,13 +3,19 @@ class Read
   def initialize(file_data)
     @file_data = file_data
     @hash = indexing
+    puts __FILE__ 
+    puts Dir.pwd
   end
 
   def indexing
-    hash = []
+    hash = {}
     @file_data.each_with_index do |line, id|
       hash[id] = line
     end
     hash
   end
 end
+file = File.open('./code/code.css')
+file_data = file.readlines.map(&:chomp)
+
+a = Read.new(file_data)
